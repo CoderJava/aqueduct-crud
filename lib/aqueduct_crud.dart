bb0ac3601bf5509c8844bc727d58aa8435d35aff
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-07-12 17:45:56 +0700

    Initial commit

---
+++ lib/aqueduct_crud.dart
@@ -0,0 +1,11 @@
+/// aqueduct_crud
+///
+/// A Aqueduct web server.
+library aqueduct_crud;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:aqueduct/aqueduct.dart';
+
+export 'channel.dart';
